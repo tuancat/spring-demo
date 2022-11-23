28e8dab5e3e1684cabb9be1a7a465bc8c9d8d744
tuan.cat <tuan.catnghiemhieu@gmail.com>
2022-11-23 14:24:05 +0700

    + update rest

--- image/src/main/java/com/sam/image/ImageApplication.java
+++ image/src/main/java/com/sam/image/ImageApplication.java
@@ -2,8 +2,10 @@ package com.sam.image;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ImageApplication {
 
 	public static void main(String[] args) {
